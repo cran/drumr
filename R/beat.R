4ed43b50effde9f1d525d30299eae45f59552445
James Martherus <james@martherus.com>
2019-10-03 05:50:12 +0000

    version 0.1.0

---
+++ R/beat.R
@@ -0,0 +1,122 @@
+#'Play a drum sound
+#'
+#'\code{beat} plays a drum beat.
+#'
+#'
+#'@param drum character string or number specifying which drum is to be played by 
+#'  specifying one of the built in sounds. The default is "snare". Every kit includes at 
+#'  least four pieces:
+#'  \enumerate{ \item \code{"kick"} \item \code{"snare"} \item \code{"hihat"}
+#'  \item \code{"crash"}}
+#'  If \code{drum} does not match any
+#'  of the options in this list, a random sound will be played.
+#'  
+#'@param kit character string specifying which kit is to be used. The default is "acoustic."
+#' Possible kits are:
+#' \enumerate{ \item \code{"acoustic"} \item \code{"hiphop"} \item \code{"electro"}
+#' \item \code{"beatbox"} \item \code{"world"} \item \code{"r2d2"}}
+#'  If \code{kit} does not match any
+#'  of the options in this list, a random sound will be played.
+#'  
+#'@param expr An optional expression to be executed before the sound.
+#'@import audio
+#'@import stringr
+#'  
+#'  
+#'@return NULL
+#'  
+#' @examples
+#' # Play a "snare" sound from the "acoustic" kit
+#' beat()
+#' 
+#' # Play a bass sound from the hiphop kit.
+#' beat(drum="kick", kit="hiphop")
+#' 
+#'@export
+beat <- function(drum="snare", kit="acoustic", expr=NULL) {
+  expr
+  drums <- c(snare = "snare.wav",
+              kick = "kick.wav",
+              hihat = "hihat.wav",
+              crash = "crash.wav")
+  kits <- c(acoustic = "acoustic/",
+            electro = "electro/",
+            hiphop = "hiphop/",
+            beatbox = "beatbox/",
+            world = "world/",
+            r2d2 = "r2d2/")
+  drum_path <- NULL
+  if(is.na(drums[drum]) || length(drums[drum]) != 1 || is.na(kits[kit]) || length(kits[kit]) != 1) {
+    if(is.character(drum)) {
+      drum <- stringr::str_trim(drum)
+      if(is.character(kit)){
+        kit <- stringr::str_trim(kit)
+        if(file.exists(system.file(paste("sounds/", kits[kit], drums[drum], sep=""), package="drumr"))){
+          drum_path <- system.file(paste("sounds/", kits[kit], drums[drum], sep=""), package="drumr")
+        } else {
+          warning(paste("Either ", '"', kit, '"', " is not a valid kit, or ", '"', drum, '"', " is not a valid drum. Playing a random sound instead.", sep = ""))
+        }
+      }
+    }
+  } else if(!file.exists(system.file(paste("sounds/", kits[kit], drums[drum], sep=""), package="drumr"))){
+      warning(paste("Either ", '"', kit, '"', " is not a valid kit, or ", '"', drum, '"', " is not a valid drum. Playing a random sound instead.", sep = ""))
+  } else {
+    drum_path <- system.file(paste("sounds/", kits[kit], drums[drum], sep=""), package="drumr")
+  }
+  
+  if(is.null(drum_path)) { # play a random sound
+    drum_path <- system.file(paste("sounds/", sample(kits, size=1), sample(drums, size=1), sep=""), package="drumr")
+  }
+  
+  tryCatch(play_file(drum_path), error = function(ex) {
+    warning("beat() could not play the sound due to the following error:\n", ex)
+  })
+}
+
+is_wav_fname <- function(fname) {
+  stringr::str_detect(fname, regex("\\.wav$", ignore_case = TRUE))
+}
+
+escape_spaces <- function(s) {
+  stringr::str_replace_all(s, " ", "\\\\ ")
+}
+
+play_vlc <- function(fname) {
+  fname <- escape_spaces(fname)
+  system(paste0("vlc -Idummy --no-loop --no-repeat --playlist-autostart --no-media-library --play-and-exit ", fname), 
+         ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  invisible(NULL)
+}
+
+play_paplay <- function(fname) {
+  fname <- escape_spaces(fname)
+  system(paste0("paplay ", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  invisible(NULL)
+}
+
+play_aplay <- function(fname) {
+  fname <- escape_spaces(fname)
+  system(paste0("aplay --buffer-time=48000 -N -q ", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  invisible(NULL)
+}
+
+play_audio <- function(fname) {
+  sfx <- audio::load.wave(fname)
+  audio::play(sfx)
+}
+
+play_file <- function(fname) {
+  if(Sys.info()["sysname"] == "Linux") {
+    if(is_wav_fname(fname) && nchar(Sys.which("paplay")) >= 1) {
+      play_paplay(fname)
+    } else if(is_wav_fname(fname) && nchar(Sys.which("aplay")) >= 1) {
+      play_aplay(fname)
+    } else if(nchar(Sys.which("vlc")) >= 1) {
+      play_vlc(fname)
+    } else {
+      play_audio(fname)
+    }
+  } else {
+    play_audio(fname)
+  }
+}
---
+++ README.md
@@ -0,0 +1,163 @@
+<!-- badges: start -->
+[![Travis build status](https://travis-ci.org/jamesmartherus/drumr.svg?branch=master)](https://travis-ci.org/jamesmartherus/drumr)
+<!-- badges: end -->
+
+# `drumr`: Turn R into a Drum Machine <img src="man/figures/logo.png" align="right" width=120 />
+
+`drumr` allows you to play drum beats from within R. At present the package contains only two functions -- `beat()`, which plays a beat from a given `kit` and `drum`, and `tempo()`, which sets the spacing between beats.
+
+## Installation
+
+To install the development version of `drumr`, use the `install_github` function from the `devtools` package:
+
+```
+library(devtools)
+install_github("jamesmartherus/drumr")
+```
+
+To install the latest release version of `drumr`, use the `install.packages` function:
+
+```
+install.packages("drumr")
+```
+
+## Requirements
+
+The `beat` function requires the `audio` package.
+
+## `beat()`
+
+`beat()` takes two arguments - `kit` and `drum`. The `kit` argument selects a drumset, and the `drum` argument selects a specific piece from that kit. As present, there are four kits available:
+
+- acoustic [(source)](https://sampleswap.org/filebrowser-new.php?d=DRUMS+%28FULL+KITS%29%2FREAL+LIVE+KITS%2FPearl+Real+Kit%2F)
+- hiphop [(source)](https://sampleswap.org/filebrowser-new.php?d=DRUMS+%28FULL+KITS%29%2FSTYLE+KITS+%28Hip+Hop%2C+Etc%29%2FHip+Hop+Specialty+Kit%2F)
+- electro [(source)](https://sampleswap.org/filebrowser-new.php?d=DRUMS+%28FULL+KITS%29%2FSTYLE+KITS+%28Hip+Hop%2C+Etc%29%2FPrecise+Electro+Percussion%2F)
+- beatbox [(source)](https://sampleswap.org/filebrowser-new.php?d=DRUMS+%28FULL+KITS%29%2FUNUSUAL+KITS+and+FX%2FBeatBox+Male%2F)
+- world [(source)](https://sampleswap.org/filebrowser-new.php?d=DRUMS+%28FULL+KITS%29%2FETHNIC+and+WORLD+PERCUSSION%2F)
+- r2d2 [(source)](https://sampleswap.org/filebrowser-new.php?d=DRUMS+%28FULL+KITS%29%2FUNUSUAL+KITS+and+FX%2FR2D2+Construction+Kit%2F)
+
+Each kit includes four pieces:
+
+- kick: a standard bass drum strike
+- snare: a standard snare drum strike
+- hihat: a closed hihat cymbal
+- crash: a crash cymbal
+
+
+### Usage
+
+`beat(kit = "acoustic", drum = "snare")`
+
+## `tempo()`
+
+`tempo()` is a basic wrapper for `Sys.time()` that sets the time between calls to `beat()` in beats per minute (bpm). `tempo()` takes one argument - `bpm`. 
+
+### Usage
+
+`tempo(bpm = 120)`
+
+
+## Example Script
+
+The following script will play a basic four-bar drum beat: 
+
+```
+# Bars 1-3
+for(i in 1:3){
+  
+  beat(drum="kick",kit="acoustic")
+  
+  tempo(240)
+  
+  beat(drum="kick",kit="acoustic")
+  
+  tempo(240)
+  
+  beat(drum="snare",kit="acoustic")
+  
+  tempo(240)
+  
+  beat(drum="kick",kit="acoustic")
+  
+  tempo(120)
+  
+  beat(drum="kick",kit="acoustic")
+  
+  tempo(240)
+  
+  beat(drum="snare",kit="acoustic")
+  
+  tempo(120)
+  
+}
+
+#Bar 4
+beat(drum="kick",kit="acoustic")
+
+tempo(240)
+
+beat(drum="kick",kit="acoustic")
+
+tempo(240)
+
+beat(drum="snare",kit="acoustic")
+
+tempo(240)
+
+beat(drum="kick",kit="acoustic")
+
+tempo(120)
+
+beat(drum="kick",kit="acoustic")
+
+tempo(240)
+
+beat(drum="snare",kit="acoustic")
+
+tempo(240)
+
+beat(drum="snare",kit="acoustic")
+
+tempo(480)
+
+beat(drum="snare",kit="acoustic")
+```
+## Example with tidyverse
+
+```
+library(tidyverse)
+library(drumr)
+
+drum <- c("kick",
+          "snare",
+          "hihat",
+          "crash")
+
+pattern1 <- tribble(~drum,~notes,
+        1,2,
+        1,2,
+        2,2,
+        1,2,
+        1,1,
+        1,2,
+        2,1) 
+
+pattern2 <- tribble(~drum,~notes,
+                 1,2,
+                 1,2,
+                 2,2,
+                 1,1,
+                 1,2,
+                 2,2,
+                 2,4) 
+
+song <- bind_rows(map_dfr(seq_len(3), ~ pattern1), pattern2 )
+
+ walk2(song$drum, song$notes,~  { beat(drum=drum[.x],kit="acoustic"); tempo(120 * .y) } )
+```
+
+# Acknowledgments
+
+- Drum samples courtesy of [sampleswap.org](https://sampleswap.org). 
+- The `beat` function uses some code from Rasmus Bååth's excellent [beepr package](https://github.com/rasmusab/beepr)
+- Drum Icon made by [Freepik](https://freepik.com) from [flaticon.com](https://www.flaticon.com)
